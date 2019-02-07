c0026f7c7e5148797eef46dea94920c02eb905f5
Barry Mcmahon <barry.mcmahon@sbxmac-users-MacBook-Pro-4.local>
2019-02-07 13:19:01 +0000

    Organiser initial setup complete

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\ApplicantContactDetails;
 use App\Models\Event;
 use App\Models\Applicant;
+use App\Models\Organiser;
 use App\Models\Slot;
 use App\Repositories\EventRepository;
 use App\Repositories\ApplicantRepository;
+use App\Repositories\OrganiserRepository;
 use App\Repositories\RepositoryInterface;
 use App\Repositories\SlotRepository;
 use Illuminate\Support\ServiceProvider;
@@ -33,5 +36,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(RepositoryInterface::class, new ApplicantRepository(new Applicant));
         $this->app->bind(RepositoryInterface::class, new EventRepository(new Event));
         $this->app->bind(RepositoryInterface::class, new SlotRepository(new Slot));
+        $this->app->bind(RepositoryInterface::class, new OrganiserRepository(new Organiser));
     }
 }
