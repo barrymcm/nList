24c07ba3ab085f1c908f7a67086149b32cd857e5
Barry McMahon <barry.mcmahon@smartbox.com>
2019-06-21 11:51:56 +0100

    [AM-14] - To fix thrown fatal exception authentication was added via middleware to resend a verifiy email

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Support\Facades\Auth;
 
 class VerificationController extends Controller
 {
@@ -35,6 +36,7 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+        $this->middleware('auth')->only('resend');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
