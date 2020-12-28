c99c68743f234ab2c6a1a764a0feb91ced90a4cc
Barry Mc Mahon <sendtobarrymcm@gmail.com>
2020-12-28 22:35:09 +0000

    #189 - Add title to create new account panel

--- src/resources/views/auth/select_account_type.blade.php
+++ src/resources/views/auth/select_account_type.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
-@section('title', 'Select account type')
+@section('title', 'Create new account')
 
 @section('content')
 
 <div class="container flex flex-col w-1/3 justify-center border-grey-400 rounded-md border-2 bg-gray-200 border-gray-300 font-thin p-10 m-auto">
+
+	<div class="container flex flex-col mb-5 text-center border-b-1">
+		<h3>Choose account type</h3>
+	</div>
 	
 	<div class="flex my-5 bg-blue-300 rounded-sm h-10 hover:bg-blue-400 transition ease-in-out duration-150 font-thin w-full">
 		<a class="py-2 text-center flex-grow" href={{ route('register', ['type' => 'organiser']) }}>Organiser</a>
