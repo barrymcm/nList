1d90607961cb2a9c5b218c07e1c13f7cd2d0458c
Barry Mc Mahon <sendtobarrymcm@gmail.com>
2020-12-28 21:47:53 +0000

    #185 - Style the select account type panel

--- src/resources/views/auth/select_account_type.blade.php
+++ src/resources/views/auth/select_account_type.blade.php
@@ -4,10 +4,15 @@
 
 @section('content')
 
-<div>
-	<a href={{ route('register', ['type' => 'organiser']) }}>Organiser</a>
-	<br><br>
-	<a href={{ route('register', ['type' => 'customer', 'list' => $list, 'event' => $event]) }}>Customer</a>
+<div class="container flex flex-col w-1/3 justify-center border-grey-400 rounded-md border-2 bg-gray-200 border-gray-300 font-thin p-10 m-auto">
+	
+	<div class="flex my-5 bg-blue-300 rounded-sm h-10 hover:bg-blue-400 transition ease-in-out duration-150 font-thin w-full">
+		<a class="py-2 text-center flex-grow" href={{ route('register', ['type' => 'organiser']) }}>Organiser</a>
+	</div>
+
+	<div class="flex my-5 bg-blue-300 rounded-sm h-10 hover:bg-blue-400 transition ease-in-out duration-150 font-thin w-full">
+		<a class="py-2 text-center flex-grow" href={{ route('register', ['type' => 'customer', 'list' => $list, 'event' => $event]) }}>Customer</a>
+	</div>
 </div>
 
 @endsection
