6c04a4e00420a6bc7b63a2166a1a6d729e6b5031
Barry Mc Mahon <sendtobarrymcm@gmail.com>
2020-12-31 02:02:23 +0000

    #196 - Add styling to the event organisers page

--- src/resources/views/event_organisers/index.blade.php
+++ src/resources/views/event_organisers/index.blade.php
@@ -4,14 +4,18 @@
 
 @section('content')
 
-    <ul>
-        @foreach($organisers as $organiser)
-            <li>Name: {{ $organiser->name }}</li>
-            <li>Description: {{ $organiser->description }}</li>
-            <br/>
-            <a href="{{ route('event_organisers.show', $organiser ) }}">View Events</a>
-            <br><br>
-        @endforeach
-    </ul>
+<div class="grid grid-rows-{{ count($organisers) }} grid-flow-row grid-cols-2 gap-6">
+	@foreach($organisers as $organiser)
+	<div class="rounded-md p-5 border-2 border-grey-400 leading-loose">
+		<ul>
+			<li>Name: {{ $organiser->name }}</li>
+			<li>Description: {{ $organiser->description }}</li>
+			<li class="flex justify-center my-5 bg-blue-300 rounded-sm h-10 hover:bg-blue-400 transition ease-in-out duration-150 font-thin">
+				<a class="w-full text-center p-1" href="{{ route('event_organisers.show', $organiser ) }}">View Events</a>
+			</li>
+		</ul>
+	</div>
+	@endforeach
+</div>
 
 @endsection
